6775b83bf8b916f2787d9f3d3d987db427d41337
Isaac <wucisaac@outlook.com>
2023-03-14 12:43:59 -0300

    lsp no funciona :c

--- init.lua
+++ init.lua
@@ -108,13 +108,4 @@ end
 ColorMyPencils()
 --fin tema
 
---telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, {})
---vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fg', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
---vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
---fin telescope
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -18,7 +18,18 @@ return{
 
   --telescope
   {'nvim-telescope/telescope.nvim',
-    tag = '0.1.0'
+    tag = '0.1.0',
+    config = function()
+      --telescope
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+      --vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+      vim.keymap.set('n', '<leader>fg', function()
+        builtin.grep_string({ search = vim.fn.input("Grep > ") })
+      end)
+      --vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+      --fin telescope
+    end,
   },
 
   --lsp para autocompletado
