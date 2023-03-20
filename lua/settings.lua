ff855ba234acba6c65a9c055de806a476c4aa8c9
Wladimir <wl.urzua@duocuc.cl>
2023-03-20 10:16:52 -0300

    wrap=false para keymap.c

--- lua/settings.lua
+++ lua/settings.lua
@@ -13,7 +13,6 @@ opt.relativenumber = true
 opt.mouse = 'a'
 opt.ignorecase = true
 opt.smartcase = true
-opt.wrap = true 
 opt.breakindent = true
 opt.tabstop = 4
 opt.shiftwidth = 4
@@ -24,3 +23,10 @@ opt.termguicolors = true
 opt.hlsearch = true --resaltado de busqueda con /palabra
 --opt.incsearch = true --resaltado de busqueda mientras se escribe con h1search false
 opt.colorcolumn = "140" --columna de marco para no exceder largo de linea	
+
+--si el buffer actual es un archivo llamado "keymap.c", entonces se setea wrap a false
+if fn.expand("%:t") == "keymap.c" then
+    opt.wrap = false
+else --si no es un archivo llamado "keymap.c", entonces se setea wrap a true
+    opt.wrap = true
+end
