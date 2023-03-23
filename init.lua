c1b92f51bc2b1385d0671811a196f7246c8d40cf
Wladimir <wl.urzua@duocuc.cl>
2023-03-22 21:21:07 -0300

    format, test easyallign

--- init.lua
+++ init.lua
@@ -46,16 +46,16 @@ lsp.configure('lua-language-server', {
 })
 
 
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp          = require('cmp')
+local cmp_select   = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-e>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-u>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+  ['<C-e>']        = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-u>']        = cmp.mapping.select_next_item(cmp_select),
+  ['<CR>']         = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"]    = cmp.mapping.complete(),
 })
 
-cmp_mappings['<Tab>'] = nil
+cmp_mappings['<Tab>']   = nil
 cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
@@ -64,19 +64,19 @@ lsp.setup_nvim_cmp({
 
 lsp.set_preferences({
     suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
+    sign_icons          = {
+        error           = 'E',
+        warn            = 'W',
+        hint            = 'H',
+        info            = 'I'
     }
 })
 
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
-  vim.keymap.set("n", "<leader>g", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "<leader>c", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>g", function() vim.lsp.buf.definition() end,   opts)
+  vim.keymap.set("n", "<leader>c", function() vim.lsp.buf.hover() end,        opts)
   vim.keymap.set("n", "<leader>x", function() vim.diagnostic.goto_next() end, opts)
 end)
 
