2278345e3086e9f416d8e532c1fbe524c1d008bc
Wladimir <wl.urzua@duocuc.cl>
2023-03-23 18:29:32 -0300

    treeseter instalado, hay que tener compiladores C

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,10 @@
+return {
+  {"nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  dependencies = {
+    "nvim-treesitter/playground",
+    "nvim-treesitter/nvim-treesitter-refactor",
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+}
+}
