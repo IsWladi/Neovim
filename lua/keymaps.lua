dd01d97a4502b3167beb6cebbb2586d97856e519
Wladimir <wl.urzua@duocuc.cl>
2023-03-27 23:42:42 -0300

    sin plugins: "no plugin", "telescope", "harpoon"

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -43,35 +43,35 @@ map('n', 's', '<Right>')
 --para comandos nativos de neovim
 
 --buffers
-map('n', '<leader>q', ':q<CR>', {desc = '[keymaps] Exit'}) --salir
-map('n', '<leader>a', ':bdelete<CR>', {desc = '[keymaps] Delete current buffer'}) --borrar buffer actual
-map('n', '<leader><leader>a', [[:lua require('utils').save_delete_buffers_except_current()<CR>]], { noremap = true, silent = true, desc = '[keymaps] Save and delete all buffers except current'})
+map('n', '<leader>q', ':q<CR>', {desc = '[no plugin] Exit'}) --salir
+map('n', '<leader>a', ':bdelete<CR>', {desc = '[no plugin] Delete current buffer'}) --borrar buffer actual
+map('n', '<leader><leader>a', [[:lua require('utils').save_delete_buffers_except_current()<CR>]], { noremap = true, silent = true, desc = '[no plugin] Save and delete all buffers except current'})
 
-map('n', '<leader>o', ':w<CR>', {desc = '[keymaps] Save'}) --guardar
-map('n', '<leader><leader>o', ':wa<CR>', {desc = '[keymaps] Save all buffers'}) --guardar todos los buffers
-map('n', '<leader>fw', [[:lua require('utils').formatWrite()<CR>]], { noremap = true, silent = true, desc = '[keymaps] Format on save' })
+map('n', '<leader>o', ':w<CR>', {desc = '[no plugin] Save'}) --guardar
+map('n', '<leader><leader>o', ':wa<CR>', {desc = '[no plugin] Save all buffers'}) --guardar todos los buffers
+map('n', '<leader>fw', [[:lua require('utils').formatWrite()<CR>]], { noremap = true, silent = true, desc = '[no plugin] Format on save' })
 
-map('n', '<leader>n', ':bnext<CR>', {desc = '[keymaps] Go to next buffer'})
-map('n', '<leader>t', ':bprevious<CR>', {desc = '[keymaps] Go to previous buffer'})
+map('n', '<leader>n', ':bnext<CR>', {desc = '[no plugin] Go to next buffer'})
+map('n', '<leader>t', ':bprevious<CR>', {desc = '[no plugin] Go to previous buffer'})
 
-map("n", "<leader>u", ':so %<CR>', {desc = '[keymaps] Reload file'}) --recargar archivo
+map("n", "<leader>u", ':so %<CR>', {desc = '[no plugin] Reload file'}) --recargar archivo
 
 --bajar y subir media pagina
-map('n', '<C-d>', '<C-d>zz', {desc = '[keymaps] Scroll down half a page and recenter'})
-map('n', '<C-u>', '<C-u>zz', {desc = '[keymaps] Scroll up half a page and recenter'})
+map('n', '<C-d>', '<C-d>zz', {desc = '[no plugin] Scroll down half a page and recenter'})
+map('n', '<C-u>', '<C-u>zz', {desc = '[no plugin] Scroll up half a page and recenter'})
 --buscar y centrar con m en vez de n
-map("n", "m", "nzz", {desc = '[keymaps] Search for next match and recenter'})
-map("n", "M", "Nzz", {desc = '[keymaps] Search for previous match and recenter'})
+map("n", "m", "nzz", {desc = '[no plugin] Search for next match and recenter'})
+map("n", "M", "Nzz", {desc = '[no plugin] Search for previous match and recenter'})
 --para normal mode, no highlight
-map("n", "<leader>nh", ":nohl<CR>", {desc = '[keymaps] Remove search highlight'})
+map("n", "<leader>nh", ":nohl<CR>", {desc = '[no plugin] Remove search highlight'})
 
 --para normal mode y visual mode, sustituciones con very very magic
-map('n', '<leader>/', ':%s/\\v//g', {desc = '[keymaps] Substitute in normal mode using very magic'})
-map('v', '<leader>/', ':s/\\v//g', {desc = '[keymaps] Substitute in visual mode using very magic'})
+map('n', '<leader>/', ':%s/\\v//g', {desc = '[no plugin] Substitute in normal mode using very magic'})
+map('v', '<leader>/', ':s/\\v//g', {desc = '[no plugin] Substitute in visual mode using very magic'})
 
 -- change tabulations
-map('v', '<', '<gv', {desc = '[keymaps] Indent left and reselect'})
-map('v', '>', '>gv', {desc = '[keymaps] Indent right and reselect'})
+map('v', '<', '<gv', {desc = '[no plugin] Indent left and reselect'})
+map('v', '>', '>gv', {desc = '[no plugin] Indent right and reselect'})
 
 --move lines
 --map('n','<A-n>',':m .+1<CR>==')
@@ -80,10 +80,10 @@ map('v', '>', '>gv', {desc = '[keymaps] Indent right and reselect'})
 --tests
 
 -- Crea un archivo
-map('n', '<leader>cf', [[:lua require('utils').create_file()<CR>]], { noremap = true, silent = true, desc = '[keymaps] Create a file' })
+map('n', '<leader>cf', [[:lua require('utils').create_file()<CR>]], { noremap = true, silent = true, desc = '[no plugin] Create a file' })
 
 -- Renombra un archivo
-map('n', '<leader>rf', [[:lua require('utils').rename_file()<CR>]], { noremap = true, silent = true, desc = '[keymaps] Rename a file' })
+map('n', '<leader>rf', [[:lua require('utils').rename_file()<CR>]], { noremap = true, silent = true, desc = '[no plugin] Rename a file' })
 
 -- Elimina un archivo
-map('n', '<leader>df', [[:lua require('utils').delete_current_file()<CR>]], { noremap = true, silent = true, desc = '[keymaps] Delete current file' })
+map('n', '<leader>df', [[:lua require('utils').delete_current_file()<CR>]], { noremap = true, silent = true, desc = '[no plugin] Delete current file' })
