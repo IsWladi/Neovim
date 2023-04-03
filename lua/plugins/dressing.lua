c8ea6fee1090bd77a8fac322c5a8315ee9d12e9f
Wladimir <wl.urzua@duocuc.cl>
2023-04-03 01:13:24 -0400

    probando funcionalidades nuevas de manfile con dressing

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -1,3 +1,69 @@
 return{
-  {"stevearc/dressing.nvim"},
+  {"stevearc/dressing.nvim",
+    config = function()
+      require('dressing').setup({
+        input = {
+          -- Set to false to disable the vim.ui.input implementation
+          enabled = true,
+
+          -- Default prompt string
+          default_prompt = "Input:",
+
+          -- Can be 'left', 'right', or 'center'
+          prompt_align = "left",
+
+          -- When true, <Esc> will close the modal
+          insert_only = true,
+
+          -- When true, input will start in insert mode.
+          start_in_insert = true,
+
+          -- These are passed to nvim_open_win
+          anchor = "SW",
+          border = "rounded",
+          -- 'editor' and 'win' will default to being centered
+          relative = "cursor",
+
+          -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+          prefer_width = 40,
+          width = nil,
+          -- min_width and max_width can be a list of mixed types.
+          -- min_width = {20, 0.2} means "the greater of 20 columns or 20% of total"
+          max_width = { 140, 0.9 },
+          min_width = { 20, 0.2 },
+
+          buf_options = {},
+          win_options = {
+            -- Window transparency (0-100)
+            winblend = 10,
+            -- Disable line wrapping
+            wrap = false,
+          },
+
+          -- Set to `false` to disable
+          mappings = {
+            n = {
+              ["<Esc>"] = "Close",
+              ["<CR>"] = "Confirm",
+            },
+            i = {
+              ["<C-c>"] = "Close",
+              ["<CR>"] = "Confirm",
+              ["<Up>"] = "HistoryPrev",
+              ["<Down>"] = "HistoryNext",
+            },
+          },
+
+          override = function(conf)
+            -- This is the config that will be passed to nvim_open_win.
+            -- Change values here to customize the layout
+            return conf
+          end,
+
+          -- see :help dressing_get_config
+          get_config = nil,
+        },
+      })
+    end,
+  },
 }
