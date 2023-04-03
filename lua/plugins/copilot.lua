2072043ab431a2e83d76fad8cdab17c12cbc4079
Wladimir <wl.urzua@duocuc.cl>
2023-04-02 23:58:43 -0400

    copilot maps cn y cp ]] [[

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,7 +2,9 @@ return{
   {'https://github.com/github/copilot.vim',
     config = function()
       vim.api.nvim_set_keymap('i', '<Tab>', '    ', {noremap = true})
-      vim.api.nvim_set_keymap('i', '<Tab>', '    ', {noremap = true})
+      vim.api.nvim_set_keymap('n', '<Space>c', ':Copilot panel<CR>', {noremap = true})
+      vim.api.nvim_set_keymap('n', '<Space>cn', ']]', {noremap = true})
+      vim.api.nvim_set_keymap('n', '<Space>cp', '[[', {noremap = true})
       vim.api.nvim_exec([[
       imap <silent><script><expr> <C-a> copilot#Accept("\<CR>")
       let g:copilot_no_tab_map = v:true
