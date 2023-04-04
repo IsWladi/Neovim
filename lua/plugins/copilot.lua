c9af4c3e2b769d8583e9b95453f163dbd473a8a2
Wladimir <wl.urzua@duocuc.cl>
2023-04-03 22:39:22 -0400

    Le agregue una descripcion a los maps de copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,14 +1,14 @@
 return{
   {'https://github.com/github/copilot.vim',
     config = function()
-      vim.api.nvim_set_keymap('i', '<Tab>', '    ', {noremap = true})
-      vim.api.nvim_set_keymap('n', '<Space>c', ':Copilot panel<CR>', {noremap = true})
-      vim.api.nvim_set_keymap('n', '<Space>cn', ']]', {noremap = true})
-      vim.api.nvim_set_keymap('n', '<Space>cp', '[[', {noremap = true})
-      vim.api.nvim_exec([[
-      imap <silent><script><expr> <C-a> copilot#Accept("\<CR>")
-      let g:copilot_no_tab_map = v:true
-]], false)
+      vim.api.nvim_set_keymap('n', '<Space>c', ':Copilot panel<CR>', {noremap = true, desc = "[copilot] panel of solutions"})
+      vim.api.nvim_set_keymap('n', '<Space>cn', ']]', {noremap = true, desc = "[copilot] next solution"})
+      vim.api.nvim_set_keymap('n', '<Space>cp', '[[', {noremap = true, desc = "[copilot] previus solution"})
+      vim.api.nvim_set_keymap('i', '<Tab>', '    ', {noremap = true, desc = "[copilot] without tab to acept solution"})
+      vim.cmd [[
+        imap <silent><script><expr> <C-a> copilot#Accept("\<CR>")
+        let g:copilot_no_tab_map = v:true
+      ]]
     end,
   }
 }
