c310c33907b64788ef04506e56ef780821832bc3
wladimir_pc2 <wl.urzua@duocuc.cl>
2023-06-05 23:07:19 -0400

    gittory ya no usa keymaps

--- lua/plugins/my_gittory.lua
+++ lua/plugins/my_gittory.lua
@@ -4,35 +4,12 @@ return{
     branch = "dev", -- for development
 
     dependencies = {
-
         {"nvim-telescope/telescope.nvim"},
         {"rcarriga/nvim-notify"},
     },
-    --[[ keys = {
-
-      { "<CR>f", function()
-                        require('gittory').search_git_root() -- For default use find_files
-                      end,
-      desc = '[telescope gittory] find files' },
-
-      { "<CR>g", function()
-                        local liveGrep = require('telescope.builtin').live_grep
-                        require('gittory').search_git_root(liveGrep)
-                      end,
-      desc = '[telescope gittory] live grep' },
-
-      { "<CR>", function()
-                  local grepString = require('telescope.builtin').grep_string
-                  local args = {use_regex = true}
-                  require('gittory').search_git_root(grepString, args)
-                end,
-      mode = "x", desc = '[telescope gittory] string grep visual mode with regex' },
-
-      { "<CR>h", function() require('gittory').telescope_home() end, -- its appear to be okey
-       desc = '[telescope gittory] find from home with telescope' },
-
-    }, ]]
-
+    init = function()
+      require('gittory').setup()
+    end,
   }
 
 }
