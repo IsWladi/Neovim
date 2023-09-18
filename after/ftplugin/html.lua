0e48c4127af3a818585398e1bc598b9ec9dacd2f
wladi <wladimir2000@protonmail.com>
2023-09-18 12:43:59 -0300

    Se cambia vim.o por vim.bo.

--- after/ftplugin/html.lua
+++ after/ftplugin/html.lua
@@ -1,4 +1,4 @@
-vim.o.expandtab = true
-vim.o.tabstop        = 4
-vim.o.softtabstop    = 4
-vim.o.shiftwidth     = 4
+vim.bo.expandtab = true
+vim.bo.tabstbop        = 4
+vim.bo.sbofttabstbop    = 4
+vim.bo.shiftwidth     = 4
--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -2,5 +2,5 @@ local set = vim.bo
 
 set.tabstop = 2
 set.softtabstop = 2
-vim.cmd('set expandtab')
 set.shiftwidth = 2
+set.expandtab = true
