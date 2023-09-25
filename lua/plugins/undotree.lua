8b5b4470012ce99fb352702a0a14507a2d68f27c
wladi <wladimir2000@protonmail.com>
2023-09-24 23:51:15 -0300

    Se corrige error en el plugin undotree.lua.

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -6,8 +6,11 @@ return {
     },
     config = function()
       vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle)
+      vim.keymap.set('n', '<leader>ut', function ()
+              vim.cmd.UndotreeToggle()
+      end)
       --focus on the window when opened, use let g:undotree_SetFocusWhenToggle = 0
-      vim.g.undotree_SetFocusWhenToggle = 1
+      -- vim.g.undotree_SetFocusWhenToggle = 1 --esto me daba error
     end,
   },
 }
