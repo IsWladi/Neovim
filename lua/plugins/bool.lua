7d08da61a3b284d88fe6ce6e1b6f4039da8e06c7
wladi <wladimir2000@protonmail.com>
2023-12-02 13:39:44 -0300

    Dias semana y meses en bool.lua

--- lua/plugins/bool.lua
+++ lua/plugins/bool.lua
@@ -6,6 +6,11 @@ return {
           increment = '<C-a>',
           decrement = '<C-x>'
         },
+        allow_caps_additions = {
+          {'enable', 'disable'},
+          {'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo'},
+          {'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'},
+        }
       })
     end
   }
