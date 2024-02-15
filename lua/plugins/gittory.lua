f88cc18e9ca2be9537cf1659e1c453269b982ce9
IsWladi <isaacx3984@outlook.com>
2024-02-15 18:08:16 -0300

    feat: set gittory to the main branch

--- lua/plugins/gittory.lua
+++ lua/plugins/gittory.lua
@@ -1,6 +1,7 @@
 return{
   -- Para setear el current working directory segun la raiz del proyecto si esta con git.
   {"IsWladi/Gittory",
+    branch = "main",
     dependencies = {
         {"rcarriga/nvim-notify"}, -- optional
     },
@@ -13,3 +14,19 @@ return{
   }
 
 }
+
+--[[ return{
+  -- Para setear el current working directory segun la raiz del proyecto si esta con git.
+  {dir = "~/workspace/Gittory/",
+    dependencies = {
+        {"rcarriga/nvim-notify"}, -- optional
+    },
+    config = true,
+
+    opts = {
+          notify = "yes",
+          atStartUp = "yes"
+    },
+  }
+
+} ]]
