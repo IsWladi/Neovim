a9cbeb5eb3c349080e66104fe0e09e7eea66ff5e
IsWladi <isaacx3984@outlook.com>
2024-02-23 19:49:35 -0300

    fix(termim.nvim): fix termim with :q! keymap

--- lua/plugins/termim.lua
+++ lua/plugins/termim.lua
@@ -2,5 +2,8 @@ return {
   {
     '2kabhishek/termim.nvim',
     cmd = { 'Fterm', 'FTerm', 'Sterm', 'STerm', 'Vterm', 'VTerm' },
+    config = function()
+      vim.keymap.set('t', '<leader>q', ":q!", {desc = '[Termim] Exit terminal mode without errors'})
+    end,
   },
 }
