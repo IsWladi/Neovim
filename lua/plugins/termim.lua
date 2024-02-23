c0d44ee988649d699ce417b0f6391c5f2f93645e
IsWladi <isaacx3984@outlook.com>
2024-02-23 20:02:59 -0300

    fix(termim.nvim): use :STerm instead :Sterm

--- lua/plugins/termim.lua
+++ lua/plugins/termim.lua
@@ -1,9 +1,8 @@
 return {
   {
     '2kabhishek/termim.nvim',
-    cmd = { 'Fterm', 'FTerm', 'Sterm', 'STerm', 'Vterm', 'VTerm' },
     config = function()
-      vim.keymap.set('t', '<leader>q', ":q!", {desc = '[Termim] Exit terminal mode without errors'})
+      vim.keymap.set('n', '<leader>l', ":STerm<CR>", {desc = '[Termim] open horizontal split terminal'})
     end,
   },
 }
