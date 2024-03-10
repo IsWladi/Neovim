10b707be443d9a197fb8896f32a93557179ffb5d
IsWladi <isaacx3984@outlook.com>
2024-03-09 23:52:45 -0300

    feat: update gittory config

--- lua/plugins/gittory.lua
+++ lua/plugins/gittory.lua
@@ -1,20 +1,44 @@
 return{
-  {dir = "~/workspace/Gittory/", -- for development purposes use local path instead of github
+  {'IsWladi/Gittory',
+    dependencies = {
+      "j-hui/fidget.nvim", -- notification plugin
+      "nvim-lua/plenary.nvim", -- to use the test suite
+    },
+    branch = "main",
+    tag = "v0.1.0",
     opts = {
           atStartUp = true,
           notifySettings = {
             enabled = true,
-            -- availableNotifyPlugins = "" -- you can change the order of the plugins
-            -- availableNotifyPlugins = {} -- you can change the order of the plugins
-            -- availableNotifyPlugins = {""} -- you can change the order of the plugins
-            -- availableNotifyPlugins =  {"print"} -- you can change the order of the plugins
-            -- availableNotifyPlugins =  {"notify"} -- you can change the order of the plugins
-            -- availableNotifyPlugins =  {"fidget"} -- you can change the order of the plugins
-            -- availableNotifyPlugins =  {"notify", "fidget"} -- you can change the order of the plugins
-            -- availableNotifyPlugins =  {"fidget", "notify"} -- you can change the order of the plugins
-            availableNotifyPlugins =  {"fidget", "notify", "print"} -- you can change the order of the plugins
+            availableNotifyPlugins =  {"fidget"},
           }
     },
   }
 
 }
+
+-- Uncomment this to be in development mode
+-- return{
+--   {dir = "~/workspace/Gittory/", -- for development purposes use local path instead of github
+--     dependencies = {
+--       "nvim-lua/plenary.nvim", -- to use the test suite
+--     },
+--     opts = {
+--           atStartUp = true,
+--           -- notifySettings = ""
+--           notifySettings = {
+--             -- enabled = false,
+--             -- availableNotifyPlugins = "" -- you can change the order of the plugins
+--             -- availableNotifyPlugins = {} -- you can change the order of the plugins
+--             -- availableNotifyPlugins = {""} -- you can change the order of the plugins
+--             -- availableNotifyPlugins =  {"print"} -- you can change the order of the plugins
+--             -- availableNotifyPlugins =  {"notify"} -- you can change the order of the plugins
+--             -- availableNotifyPlugins =  {"fidget"} -- you can change the order of the plugins
+--             -- availableNotifyPlugins =  {"notify", "fidget"} -- you can change the order of the plugins
+--             -- availableNotifyPlugins =  {"fidget", "notify"} -- you can change the order of the plugins
+--             availableNotifyPlugins =  {"fidget", "notify", "print"} -- you can change the order of the plugins
+--           }
+--     },
+--   }
+--
+-- }
