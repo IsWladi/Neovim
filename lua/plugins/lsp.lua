2c9885c61703edf0e8ef11ef1013f54eb6a50be2
IsWladi <isaacx3984@outlook.com>
2024-09-08 15:55:05 -0300

    feat: disable cmp on cmdline searchs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -140,12 +140,12 @@ return{
       require("cmp_git").setup() -- setup cmp-git
 
       -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline({ '/', '?' }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'buffer' }
-        }
-      })
+      -- cmp.setup.cmdline({ '/', '?' }, {
+      --   mapping = cmp.mapping.preset.cmdline(),
+      --   sources = {
+      --     { name = 'buffer' }
+      --   }
+      -- })
 
       -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
       cmp.setup.cmdline(':', {
