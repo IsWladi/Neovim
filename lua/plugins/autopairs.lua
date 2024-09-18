50fc594a3348794d4400346ae80732f32fa3a568
IsWladi <wladimir2000@protonmail.com>
2024-09-18 18:53:49 -0300

    feat: replace autopair plugin

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,19 +1,7 @@
 return {
-  {
-    'altermo/ultimate-autopair.nvim',
-    enabled = true,
-    event={'InsertEnter','CmdlineEnter'},
-    branch='v0.6', --recommended as each new version will have breaking changes
-    opts={
-      -- fastwarp make some freaky things with the pairs, so I disabled it because I don't use it
-      fastwarp={
-        enable=false,
-        map='<A-f>', --string or table
-        rmap='<A-F>', --string or table
-        cmap='<A-f>', --string or table
-        rcmap='<A-F>', --string or table
-      }
-    },
-  }
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
 }
-
